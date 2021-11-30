866ca0e281ed896d41892cbf3ac8e13cfc6adaf4
Sungwoo Heo <npnghsw1216@naver.com>
2021-11-30 18:06:26 +0900

    6. admin에 등록

--- myhomework11/homework/admin.py
+++ myhomework11/homework/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from homework.models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["dog_name", "dog_size", "dog_origin", "dog_explain", "dog_caution", "created_at"]
+    search_fields = ["dog_name"]
+
+admin.site.register(Post, PostAdmin)
--- myhomework11/homework/models.py
+++ myhomework11/homework/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 
 class Post(models.Model):
-    author_name = models.CharField(max_length=20)
-    title = models.CharField(max_length=200)
-    content = models.TextField()
+    # list_display = ["dog_name", "dog_size", "dog_orgin", "dog_explain", "dog_caution", "created_at"]
+    # search_fields = ["dog_name"]
+    dog_name = models.CharField(max_length=20)
+    dog_size = models.CharField(max_length=20)
+    dog_origin = models.CharField(max_length=20)
+    dog_explain = models.TextField()
+    dog_caution = models.TextField()
     #  upload_to
     #  - 문자열 : 파일이 저장되는 폴더의 경로
     photo = models.ImageField(blank=True)  # , upload_to='blog/post/%Y/%m/%d/%H')
