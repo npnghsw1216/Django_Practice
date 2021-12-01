99275a35a5872030d92ec8503b9fb455851e5704
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-01 14:39:10 +0900

    6. 개발모드에서 sql 환경

--- mydjango13/mydjango13/settings.py
+++ mydjango13/mydjango13/settings.py
@@ -142,3 +142,22 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+if DEBUG:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+            },
+        },
+        "loggers": {
+            "django.db.backends": {
+                "handlers": ["console"],
+                "level": "DEBUG",
+                "propagate": False,
+            },
+        },
+    }
