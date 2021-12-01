51e8efd5c002dd07b06f8e48bd210955a3c79fc7
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-01 11:06:20 +0900

    3. mall/shop 모델 및 마이그에션 생성

--- mydjango13/mall/models.py
+++ mydjango13/mall/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    # id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
