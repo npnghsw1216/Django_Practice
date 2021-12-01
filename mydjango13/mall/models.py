5971434fdc6cb6a838dc0b847bad35078d68b1da
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-01 13:14:20 +0900

    mall/ shop에 migration 추가

--- mydjango13/mall/models.py
+++ mydjango13/mall/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 
@@ -5,6 +7,8 @@ class Shop(models.Model):
     # id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=13)
+    open_time = models.TimeField(default=datetime.time())
     photo = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
