28e35b2b72a9feef6fc27f6a998666420bff8abd
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-01 19:23:46 +0900

    5. mall/Shop 모델 및 마이그레이션 생성

--- myhomework13/mall/models.py
+++ myhomework13/mall/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    photo = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
