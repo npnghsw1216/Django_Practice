2cbbc035584d623f53f39a195a95203079e0ad4f
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-02 13:23:17 +0900

    bootstrap 5 top navigation 예제 활용

--- mydjango14/mydjango14/urls.py
+++ mydjango14/mydjango14/urls.py
@@ -17,13 +17,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from catube.views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catube/', index),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
-                        document_root=settings.MEDIA_ROOT)
+                      document_root=settings.MEDIA_ROOT)
 
 
 if settings.DEBUG:
