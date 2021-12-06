389ce880150291fe9a79861514b4998243d63f84
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-07 00:13:02 +0900

    7. 위 모델에 대한 _list, _detail, layout 페이지 구현

--- myhomework16/friendsmap/views.py
+++ myhomework16/friendsmap/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.shortcuts import render
+from friendsmap.models import Post
 
-# Create your views here.
+
+def map_list(request: HttpRequest) -> HttpResponse:
+
+    qs = Post.objects.all()  # QuerySet Type
+    qs = qs.order_by("-pk")
+
+    return render(request, "friendsmap/map_list.html")
+
+
+def map_detial(request: HttpRequest, pk=int) -> HttpResponse:
+    post = Post.objects.get(pk=pk)
+    return render(request, "friendsmap/map_detail.html", {
+        "post": post,
+    })
