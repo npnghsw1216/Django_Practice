a900a21f4fe77625ce39621deb7fe0aca53a576f
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-07 00:51:39 +0900

    10. 목록 페이지에서 <form> 을 활용한 간단 검색

--- myhomework16/friendsmap/views.py
+++ myhomework16/friendsmap/views.py
@@ -1,18 +1,27 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from django.shortcuts import render
 from friendsmap.models import Post
 
 
 def map_list(request: HttpRequest) -> HttpResponse:
 
     qs = Post.objects.all()  # QuerySet Type
-    qs = qs.order_by("-pk")
+    qs = qs.order_by("pk")
 
-    return render(request, "friendsmap/map_list.html")
+    query = request.GET.get("query", "")
+
+    if query:
+        qs = qs.filter(title__icontains=query)
+
+    context_data = {
+        "map_list": qs
+    }
+
+    return render(request, "friendsmap/map_list.html", context_data)
 
 
 def map_detail(request: HttpRequest, pk=int) -> HttpResponse:
+
     post = Post.objects.get(pk=pk)
     return render(request, "friendsmap/map_detail.html", {
         "post": post,
