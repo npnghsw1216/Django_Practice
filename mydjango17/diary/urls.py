1bad6ef641acb0d836c1f1ab6df1914a8edead7d
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-07 10:41:02 +0900

    diary앱 생성 및 등록

---
+++ mydjango17/diary/urls.py
@@ -0,0 +1,5 @@
+from diary import views
+
+app_name = "diary"
+
+urlpatterns = []
--- mydjango17/mydjango17/urls.py
+++ mydjango17/mydjango17/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('diary/', include('diary.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
