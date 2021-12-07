ab5559da603de90e9c39c0f4caed002752cd1447
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-07 23:17:57 +0900

    6. 장고 모델 생성 및 마이그레이션 수행 unique한모델 이름, 필드

--- myhomework17/news/models.py
+++ myhomework17/news/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        # 추상 클래스로서, DB 테이블이 생기지 않는다.
+        # 부모로서만 존재해서
+
+
+class Post(TimeStampedModel):
+    author_name = models.CharField(max_length=20)
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to="news/post/%Y/%m/%d")  # 연, 월, 일로 나눠서 저장
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Comment(TimeStampedModel):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_name =models.CharField(max_length=20)
+    message = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Tag(TimeStampedModel):
+    name = models.CharField(max_length=200, db_index=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
