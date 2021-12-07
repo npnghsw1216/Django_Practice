e93af70ab39ae0814ec5234fdcd2de8a3d23e32a
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-07 14:06:56 +0900

    첫화면 페이지

--- mydjango17/mydjango17/urls.py
+++ mydjango17/mydjango17/urls.py
@@ -16,11 +16,20 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+
+
+# TODO : 편의상 여기에 root를 구현하지만,
+# 차후 RedirectView CBV를 써서 이 구현을 제거할 예정
+def root(request):
+    return redirect("diary:post_list")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('diary/', include('diary.urls'))
+    path('diary/', include('diary.urls')),
+    path('', root, name="root"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
