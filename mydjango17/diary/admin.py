4fb7412328857615b83d69e769e21e63a3b57cd7
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-07 11:03:07 +0900

    diary 모델 생성(realation)

--- mydjango17/diary/admin.py
+++ mydjango17/diary/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from diary.models import Post, Comment, Tag
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
--- mydjango17/diary/models.py
+++ mydjango17/diary/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # 추상 클래스로서, DB 테이블이 생기지 않는다. (부모로서만 존재)
+
+
+class Post(TimeStampedModel):
+    author_name = models.CharField(max_length=20)
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to="diary/post/%Y/%m/%d")  # 연, 월, 일로 나눠서 저장
+    tag_set = models.ManyToManyField("Tag", blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "포스팅"
+        verbose_name_plural = "포스팅1"
+
+
+class Comment(TimeStampedModel):
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
+
+    class Meta:
+        verbose_name = "포스팅2"
+        verbose_name_plural = "포스팅2"
+
+
+class Tag(TimeStampedModel):
+    name = models.CharField(max_length=200, db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "포스팅3"
+        verbose_name_plural = "포스팅3"
