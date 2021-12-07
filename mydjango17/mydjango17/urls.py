b6e6ff8e80b62eeac418e6b1e37dc320c572cc69
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-07 10:35:24 +0900

    MEDIA 설정

--- mydjango17/mydjango17/urls.py
+++ mydjango17/mydjango17/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,8 +22,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    import  debug_toolbar
+    import debug_toolbar
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
     ]
