8ffedecdaddaeba9a5c8b0f5d48a8673e4e01c3d
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-09 16:41:02 +0900

    ~~~

--- mydjango19/mydjango17/settings.py
+++ mydjango19/mydjango17/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # third apps
+    "bootstrap5",
     # local apps
     'diary',
 ]
@@ -117,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'UTC'
 
