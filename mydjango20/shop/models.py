1f0b75fe8db119281ad2fc41a9dcb5381fe9d7d4
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-10 16:29:43 +0900

    shop앱 Shop/Review/Tag 모델/마이그레이션 생성

--- mydjango20/shop/models.py
+++ mydjango20/shop/models.py
@@ -1,3 +1,34 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Shop(TimestampedModel):
+    name = models.CharField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    telephone = models.CharField(max_length=14,
+                                 validators=[
+                                     RegexValidator(r"^\d{3}-?\d{4}-?\d{4}$", message="전화번호를 입력해주세요."),
+                                 ],
+                                 help_text="입력 예) 042-1234-1234")
+
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+
+class Review(TimestampedModel):
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+
