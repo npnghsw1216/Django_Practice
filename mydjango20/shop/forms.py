96ce4822126efd8ef5e761e8c204253c135c5cd5
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-13 16:12:47 +0900

    shop_new 뷰에 구현된 Tag 저장 로직을 ShopForm 클래스로 이동

--- mydjango20/shop/forms.py
+++ mydjango20/shop/forms.py
@@ -1,8 +1,27 @@
 from django import forms
-from shop.models import Shop, Review
+from shop.models import Shop, Review, Tag
 
 
 class ShopForm(forms.ModelForm):
+    tags = forms.CharField()
+
+    def save(self):
+        # 부모의 save를 호출해주어야 한다.
+        saved_post = super().save()
+
+        #  부가적인 연산을 수행할 수 있다.
+        tag_list = []
+        tags = self.cleaned_data.get("tags", "")
+        for word in tags.split(","):
+            tag_name = word.strip()
+            tag, __ = Tag.objects.get_or_create(name=tag_name)
+            tag_list.append(tag)
+
+        saved_post.tag_set.clear()  # 간단구현을 위해 clear 호출
+        saved_post.tag_set.add(*tag_list)
+
+        return saved_post
+
     class Meta:
         model = Shop
         fields = [
--- mydjango20/shop/views.py
+++ mydjango20/shop/views.py
@@ -56,16 +56,6 @@ def shop_new(request: HttpRequest) -> HttpResponse:
         if form.is_valid():
             saved_post = form.save()
 
-            tag_list = []
-            tags = form.cleaned_data.get("tags", "")
-            for word in tags.split(","):
-                tag_name = word.strip()
-                tag, __ = Tag.objects.get_or_create(name=tag_name)
-                tag_list.append(tag)
-
-            saved_post.tag_set.clear()  # 간단구현을 위해 clear 호출
-            saved_post.tag_set.add(*tag_list)
-
             # shop_detail 뷰를 구현했다면 !!!
             return redirect("shop:shop_detail", saved_post.pk)
     else:
