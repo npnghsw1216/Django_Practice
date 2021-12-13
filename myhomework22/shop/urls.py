29a198ef83b2028e839e70cda7fb3d33368349be
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-14 02:10:03 +0900

    layout, shop_detail, shop_list 페이지 구현

--- myhomework22/shop/urls.py
+++ myhomework22/shop/urls.py
@@ -1,10 +1,11 @@
-# from django.urls import path
-#
-# from shop import views
-#
-# app_name = "shop"
-#
-#
-# urlpatters =[
-#     path("", views.shop_list, name="shop_list")
-# ]
+from django.urls import path
+
+from shop import views
+
+app_name = "shop"
+
+
+urlpatterns = [
+    path("", views.shop_list, name="shop_list")
+    path("<int:pk>/", views.shop_detail, name="shop_detail")
+]
--- myhomework22/shop/views.py
+++ myhomework22/shop/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from shop.models import Shop, Category
+
+
+def shop_list(request: HttpRequest) -> HttpResponse:
+    qs = Shop.objects.all()
+    category_qs = Category.objects.all()
+    category_id:str = request.GET.get("category_id", "")
+    if category_id:
+        qs = qs.filter(category__pk=category_id)
+
+    return render(request, "shop/shop_list.html", {
+        "shop_list": qs,
+        "category_list": category_qs,
+    })
+
+
+def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+
+    return render(request, "shop/shop_detail.html", {
+        "shop": shop,
+    })
