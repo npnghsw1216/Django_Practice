931afac93586ff61d9c9878af64e73441e48e4aa
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-14 02:55:51 +0900

    shop_detail, shop_list 페이지 수정 및 url 연결

--- myhomework22/shop/urls.py
+++ myhomework22/shop/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.shop_list, name="shop_list"),
     path("<int:pk>/", views.shop_detail, name="shop_detail"),
     path("new/", views.shop_new, name="shop_new"),
+    path("<int:pk>/edit/", views.shop_edit, name="shop_edit"),
 ]
--- myhomework22/shop/views.py
+++ myhomework22/shop/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from shop.form import ShopForm
 from shop.models import Shop, Category
@@ -7,14 +7,18 @@ from shop.models import Shop, Category
 
 def shop_list(request: HttpRequest) -> HttpResponse:
     qs = Shop.objects.all()
-    category_qs = Category.objects.all()
-    category_id:str = request.GET.get("category_id", "")
+
+    category_id: str = request.GET.get("category_id", "")
     if category_id:
         qs = qs.filter(category__pk=category_id)
 
+    query = request.GET.get("query", "")
+    if query:
+        qs = qs.filter(title__icontains=query)
+
     return render(request, "shop/shop_list.html", {
         "shop_list": qs,
-        "category_list": category_qs,
+        "category_list": qs,
     })
 
 
@@ -27,8 +31,29 @@ def shop_detail(request: HttpRequest, pk: int) -> HttpResponse:
 
 
 def shop_new(request: HttpRequest) -> HttpResponse:
-    form = ShopForm()
+    if request.method == "POST":
+        form = ShopForm(request.POST, request.FILES)
+        if form.is_valid():
+            saved_post = form.save()
+
+            return redirect("shop:shop_detail", saved_post.pk)
+    else:
+        form = ShopForm()
     return render(request, "shop/shop_form.html", {
         "form": form,
     })
 
+
+def shop_edit(request: HttpRequest, pk: int) -> HttpResponse:
+    shop = get_object_or_404(Shop, pk=pk)
+    if request.method == "POST":
+        form = ShopForm(request.POST, request.FILES, instance=shop)
+        if form.is_valid():
+            saved_shop = form.save()
+            return redirect("shop:shop_detail", saved_shop.pk)
+    else:
+        form = ShopForm(instance=shop)
+
+    return render(request, "shop/shop_form.html", {
+        "form": form,
+    })
