409fe05fce5dc8070b12b17c546c8516d8215d1b
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-13 14:24:48 +0900

    django-bootstrap v5 설정

--- mydjango20/mydjango20/settings.py
+++ mydjango20/mydjango20/settings.py
@@ -19,13 +19,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third apps
     "bootstrap5",
+    # local apps
     'shop',
 ]
 
