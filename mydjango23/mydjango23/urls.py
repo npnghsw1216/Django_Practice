0caf3999a6d52630f67f9f688508d2eba76ef5d9
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-15 10:42:25 +0900

    blog 앱 생성 및 등록

--- mydjango23/mydjango23/urls.py
+++ mydjango23/mydjango23/urls.py
@@ -17,9 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('', RedirectView.as_view(url='/blog/'), name='root')
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
