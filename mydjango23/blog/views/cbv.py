57acab92eb7c2e85a094f4a5d79ba8b1ed1b7cbd
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-15 14:43:52 +0900

    PostCreateView/PostUpdateView 에서 post_detail로의 success_url 지정

--- mydjango23/blog/views/cbv.py
+++ mydjango23/blog/views/cbv.py
@@ -14,21 +14,38 @@ post_detail = DetailView.as_view(
     model=Post,
 )
 
-post_new = CreateView.as_view(
-    model=Post,
-    form_class=PostForm,
-    success_url=reverse_lazy("blog:post_list")
-)
 
-post_edit = UpdateView.as_view(
-    model=Post,
-    form_class=PostForm,
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    # success_url = reverse_lazy("blog:post_list")
+
+    def get_success_url(self):
+        # self.object  # 저장된 모델 인스턴스
+        post_pk = self.object.pk
+        return reverse("blog:post_detail", args=[post_pk])
+        # return resolve_url("blog:post_detail", post_pk)
+        # return redirect ("blog:post_detail", post_pk)
+
+
+post_new = PostCreateView.as_view()
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+
+    def get_success_url(self):
+        post_pk = self.object.pk
+        return reverse("blog:post_detail", args=[post_pk])
+
+
+post_edit = PostUpdateView.as_view(
     # TODO: 가변적으로 URL을 지정할 수 없다.
     # TODO: URL Reverse가 미적용
     # success_url="/blog/"
     # success_url="blog:post_list", # URL Reverse 미지원
     # success_url=reverse("blog:post_list"),
-    success_url=reverse_lazy("blog:post_list")
 )
 
 post_delete = DeleteView.as_view(
