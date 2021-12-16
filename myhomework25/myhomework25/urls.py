83300bfee396f6b421de8824a7f87515fc2a1bc8
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-16 22:21:30 +0900

    MEDIA 설정

--- myhomework25/myhomework25/urls.py
+++ myhomework25/myhomework25/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
+
 
 if settings.DEBUG:
     import debug_toolbar
