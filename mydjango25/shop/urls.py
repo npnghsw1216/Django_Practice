22341d9de54b58729436dd98f71425360db6f6d9
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-17 14:49:50 +0900

    shop_list, shop_detail 뷰를 CBV로 구현

--- mydjango25/shop/urls.py
+++ mydjango25/shop/urls.py
@@ -1,3 +1,10 @@
+from django.urls import path
+
+from shop import views
+
 app_name = "shop"
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.shop_list, name="shop_list"),
+    path("<int:pk>/", views.shop_detail, name="shop_detail"),
+]
--- mydjango25/shop/views.py
+++ mydjango25/shop/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from shop.models import Shop, Category
+
+
+class ShopListView(ListView):
+    model = Shop
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data["category_list"] = Category.objects.all()
+        return context_data
+
+
+shop_list = ShopListView.as_view()
+
+shop_detail = DetailView.as_view(
+    model=Shop,
+)
