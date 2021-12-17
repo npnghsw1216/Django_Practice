38db90288e3d3cd5024470c63df43e64e76cf1ab
Sungwoo Heo <npnghsw1216@naver.com>
2021-12-17 11:24:28 +0900

    회원가입, 로그인 구현

--- mydjango25/accounts/views.py
+++ mydjango25/accounts/views.py
@@ -1,21 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 signup = CreateView.as_view(
     form_class=UserCreationForm,
     success_url=reverse_lazy("accounts:login"),
-    # template_name="",
+    template_name="accounts/signup_form.html",
 )
 
 
-def signup(request):
-    pass
-
-
-def login(request):
-    pass
+login = LoginView.as_view(
+    template_name="accounts/login_form.html",
+)
 
 
 def profile(request):
